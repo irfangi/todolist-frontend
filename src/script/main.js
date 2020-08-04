5f61864229ae393ab2d901d8351169c56f4fc2cf
IT DEV <itdev@admins-MacBook-Air.local>
2020-08-04 17:34:15 +0700

    add detail list

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,12 @@
+import Swal from 'sweetalert2'
+
 const main = () =>{
     //element
     const allList = document.querySelectorAll('.todo-list')
     const createNewTodo = document.getElementById('createNewTodo')
     const cancelInputTodo = document.getElementById('cancelInputTodo')
     const modalInput = document.getElementById('modalInputTodo')
+    const modalDetailTodo = document.getElementById('modalDetailTodo')
 
     //function
     const toggleModalInputTodo = () =>{
@@ -23,8 +26,41 @@ const main = () =>{
             node.classList.toggle('active')
             actionButton.classList.toggle('hidden')
         })
+        node.querySelector('h4.text-sm').addEventListener('click', ()=>{
+            modalDetailTodo.classList.toggle('active')
+        })
+        node.querySelector('i.icon-trash-empty').addEventListener('click', ()=>{
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+              }).then((result) => {
+                if (result.value) {
+                  Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                  )
+                }
+              })
+        })
+        node.querySelector('i.icon-check').addEventListener('click', () =>{
+            alert('done')
+        })
+    })
+
+    modalDetailTodo.querySelector('.button-close-modal').addEventListener('click', () =>{
+        modalDetailTodo.classList.toggle('active')
     })
     
+    modalInput.querySelector('.button-close-modal').addEventListener('click', () =>{
+        modalInput.classList.toggle('active')
+    })
+
     createNewTodo.addEventListener('click', toggleModalInputTodo)
     cancelInputTodo.addEventListener('click', toggleModalInputTodo)
 
